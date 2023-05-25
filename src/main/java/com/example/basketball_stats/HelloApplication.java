d225335bbbe8455367e1db3862b02056ba63fbb2
Gixter02 <mattia.montanari2002@gmail.com>
2023-05-25 12:28:19 +0200

    Welcome window not resizable, added Application Image

--- src/main/java/com/example/basketball_stats/HelloApplication.java
+++ src/main/java/com/example/basketball_stats/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.basketball_stats;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +14,9 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
+        stage.getIcons().add(new Image("com/example/basketball_stats/images/basketball_ball.png"));
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
