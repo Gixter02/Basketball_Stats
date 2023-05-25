5b3725a20f5caad074ebb3715fe185765d382b3f
Gixter02 <mattia.montanari2002@gmail.com>
2023-05-25 20:36:20 +0200

    Implemented quarter selection and filter selection

--- src/main/java/com/example/basketball_stats/HelloApplication.java
+++ src/main/java/com/example/basketball_stats/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.basketball_stats;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -11,7 +12,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Basketball Stats");
         stage.getIcons().add(new Image("com/example/basketball_stats/images/basketball_ball.png"));
--- src/main/java/com/example/basketball_stats/HelloController.java
+++ src/main/java/com/example/basketball_stats/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.basketball_stats;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private ComboBox quarterComboBox;
+    @FXML
+    private ComboBox filterBox;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void initialize() {
+        // Get the list of items and add new items
+        quarterComboBox.getItems().addAll("1°Q","2°Q","3°Q","4°Q");
+        filterBox.getItems().addAll("1°Q","2°Q","3°Q","4°Q","1°Half","2°Half","Game","Player...");
     }
+    @FXML
+    void selectQuarter(ActionEvent event) {
+
+    }
+    @FXML
+    void filterData(ActionEvent event) {
+
+    }
+
 }
