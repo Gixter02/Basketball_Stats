c953539ced18f4d4d8afc59c031ccf30be93c60f
Gixter02 <mattia.montanari2002@gmail.com>
2023-05-26 16:47:13 +0200

    Added buttons to add 2 point shot, added the method to get the position of the shot

--- src/main/java/com/example/basketball_stats/HelloController.java
+++ src/main/java/com/example/basketball_stats/HelloController.java
@@ -3,12 +3,23 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
 public class HelloController {
+
+    @FXML
+    private Label twoPontersShot;
+    @FXML
+    private Button made2Shot;
+
+    @FXML
+    private Button missed2Shot;
     @FXML
     private BorderPane borderPane;
 
@@ -18,6 +29,11 @@ public class HelloController {
     private ComboBox<String> quarterComboBox;
 
 
+    public int attemptedShot = 0;
+    public int madeShot = 0;
+
+    public double x = 0.0;
+    public double y = 0.0;
 
 
     @FXML
@@ -25,10 +41,29 @@ public class HelloController {
         // Get the list of items and add new items
         quarterComboBox.getItems().addAll("1°Q","2°Q","3°Q","4°Q");
 
+        twoPontersShot.setText(String.valueOf(madeShot) + "/" + String.valueOf(attemptedShot));
     }
     @FXML
     void selectQuarter(ActionEvent event) {
 
+    }
+    @FXML
+    void made2PShot(ActionEvent event) {
+        attemptedShot++;
+        madeShot++;
+        twoPontersShot.setText(String.valueOf(madeShot) + "/" + String.valueOf(attemptedShot));
+    }
+
+    @FXML
+    void missed2PShot(ActionEvent event) {
+        attemptedShot++;
+        twoPontersShot.setText(String.valueOf(madeShot) + "/" + String.valueOf(attemptedShot));
+    }
+    @FXML
+    void handleClick(MouseEvent event) {
+        x = event.getX();
+        y = event.getY();
+        System.out.println("Mouse clicked at: (" + x + ", " + y + ")");
     }
 
 
