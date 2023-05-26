b3976854f16d9d19232f4ae03e8c52933b05b809
Gixter02 <mattia.montanari2002@gmail.com>
2023-05-26 15:45:59 +0200

    Window dimension forced, added View

--- src/main/java/com/example/basketball_stats/HelloController.java
+++ src/main/java/com/example/basketball_stats/HelloController.java
@@ -4,26 +4,32 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 
 public class HelloController {
     @FXML
-    private ComboBox quarterComboBox;
+    private BorderPane borderPane;
+
     @FXML
-    private ComboBox filterBox;
+    private ImageView courtImage;
+    @FXML
+    private ComboBox<String> quarterComboBox;
+
+
+
 
     @FXML
     private void initialize() {
         // Get the list of items and add new items
         quarterComboBox.getItems().addAll("1°Q","2°Q","3°Q","4°Q");
-        filterBox.getItems().addAll("1°Q","2°Q","3°Q","4°Q","1°Half","2°Half","Game","Player...");
+
     }
     @FXML
     void selectQuarter(ActionEvent event) {
 
     }
-    @FXML
-    void filterData(ActionEvent event) {
 
-    }
 
 }
