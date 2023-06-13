11fe104126596b600e5b2419d15f4c675bb0254f
karim <damorekarim7@gmail.com>
2023-06-13 17:25:27 +0200

    Team insertion dialog controller

---
+++ src/main/java/com/example/basketball_stats/TeamInsertionDialogController.java
@@ -0,0 +1,40 @@
+package com.example.basketball_stats;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+
+public class TeamInsertionDialogController {
+
+    @FXML
+    private Button addPlayerButton;
+
+    @FXML
+    private TextField firstNameTextfield;
+
+    @FXML
+    private TextField lastNameTextfield;
+
+    @FXML
+    private TextField numberOfPlayer;
+
+    @FXML
+    private Label numberOfPlayersAddedLabel;
+
+    @FXML
+    private ComboBox<?> numberOfPlayersToAddCombobox;
+
+
+    @FXML
+    public void initialize(){};
+
+
+
+
+}
+
+
